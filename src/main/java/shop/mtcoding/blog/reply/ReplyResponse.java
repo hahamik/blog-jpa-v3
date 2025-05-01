da4d9589dcb66982f3b98bd079ef8d048c470e7f
hahamik <dlapdl0615@gmail.com>
2025-05-01 10:32:14 +0900

    DTO 전환 완료

--- src/main/java/shop/mtcoding/blog/reply/ReplyResponse.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyResponse.java
@@ -1,4 +1,26 @@
 package shop.mtcoding.blog.reply;
 
+import lombok.Data;
+
+import java.sql.Timestamp;
+
 public class ReplyResponse {
+
+    @Data
+    public static class DTO{
+        private Integer id;
+        private String content;
+        private Integer userId;
+        private Integer boardId;
+        private String createdAt; // 날짜는 String으로 return한다.
+
+
+        public DTO(Reply reply) {
+            this.id = reply.getId();
+            this.content = reply.getContent();
+            this.userId = reply.getUser().getId();
+            this.boardId = reply.getBoard().getId();
+            this.createdAt = reply.getCreatedAt().toString();
+        }
+    }
 }
